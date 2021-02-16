eecb745c391bc0dd0388b417fa97f4c46bcf2533
Joel E. Rego <joelerego@gmail.com>
2021-02-15 20:43:37 -0800

    Brought shared state to a higher level in the hierarchy.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './Components/MenuComponent';
+import { DISHES } from './Shared/dishes';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark={true} color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark={true} color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
